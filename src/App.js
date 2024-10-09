c79fa78435349e7fa2cf7217ce0f18ae25576220
jocatins <jocakhamen@gmail.com>
2024-10-09 03:06:55 +0100

    tailwind-css-integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import PokemonSearch from './PokemonSearch';
-import React from 'react';
+import PokemonSearch from "./PokemonSearch";
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      <PokemonSearch />
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<PokemonSearch />
+		</div>
+	);
 }
 
 export default App;
