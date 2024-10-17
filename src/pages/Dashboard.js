139829ec7599a2244ef10069d1d47472a67fd2f7
jocatins <jocakhamen@gmail.com>
2024-10-17 22:21:49 +0100

    dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom";
+import BuyerProfilePieChart from "../components/dashboard/BuyerProfilePieChart";
+import DashboardStatsGrid from "../components/dashboard/DashboardStatsGrid";
+import PopularProducts from "../components/dashboard/PopularProducts";
 import React from "react";
+import RecentOrders from "../components/dashboard/RecentOrders";
+import TransactionChart from "../components/dashboard/TransactionChart";
 
 function Dashboard() {
 	return (
-		<>
-			<div>Dashboard</div>
-			<Link to="/about">About</Link>
-		</>
+		<div className="flex flex-col gap-4">
+			<DashboardStatsGrid />
+			<div className="flex flex-row gap-4 w-full">
+				<TransactionChart />
+				<BuyerProfilePieChart />
+			</div>
+			<div className="flex flex-row gap-4 w-full">
+				<RecentOrders />
+				<PopularProducts />
+			</div>
+		</div>
 	);
 }
 
